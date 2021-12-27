4b3dd928f2b40392fd069ac50735ad68f8a1f4c6
vitorim <vitintra63@gmail.com>
2021-12-27 20:04:14 -0300

    feat: forgot password email

---
+++ test/users/passwords.spec.ts
@@ -0,0 +1,37 @@
+import Mail from "@ioc:Adonis/Addons/Mail";
+import Database from "@ioc:Adonis/Lucid/Database";
+import { UserFactory } from "Database/factories";
+import test from "japa";
+import supertest from "supertest";
+
+const baseUrl = `http://${process.env.HOST}:${process.env.PORT}`;
+
+test.group("Password", (group) => {
+  test("it should send an email with forgot password instructions", async (assert) => {
+    const { email, username } = await UserFactory.create();
+
+    // Mail.trap((message) => {
+    //   assert.deepEqual(message.to, [{ address: email }]);
+    //   assert.deepEqual(message.from, { address: "no-reply@roleplay.com" });
+    //   assert.equal(message.subject, "Roleplay: Recuperação de Senha");
+    //   assert.include(message.html!, username);
+    // });
+
+    await supertest(baseUrl)
+      .post("/forgot-password")
+      .send({
+        email: email,
+        resetPasswordUrl: "url",
+      })
+      .expect(204);
+
+    // Mail.restore();
+  });
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction();
+  });
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction();
+  });
+});
