ea9c57e8eb250484699f0d85e7714aef70bdde56
vintra <vitintra63@gmail.com>
2022-03-07 00:33:00 -0300

    feat: update route

--- start/routes.ts
+++ start/routes.ts
@@ -34,6 +34,7 @@ Route.post("/sessions", "SessionsController.store");
 Route.delete("/sessions", "SessionsController.destroy");
 
 Route.post("/groups", "GroupsController.store").middleware("auth");
+Route.patch("/groups/:id", "GroupsController.patch"); //.middleware("auth");
 
 Route.get("/groups/:groupId/requests", "GroupRequestsController.index").middleware("auth");
 Route.post("/groups/:groupId/requests", "GroupRequestsController.store").middleware("auth");
